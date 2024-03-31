50e77cc8d466dd4f19a332c4df99514a04334358
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2024-03-31 10:51:29 -0700

    automated routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Link from "next/link";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/auth/login">Login</Link>
-            </li>
-            <li>
-              <Link href="/auth/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/routes/navbarRoutes.ts
@@ -0,0 +1,19 @@
+export const navbarRoutes = [
+  {
+    href: "/dashboard",
+    text: "Dashboard",
+    // role: ["admin","moderador"] Lo va a ver un admin y moderador, se hace con condicional para controlarlo
+  },
+  {
+    href: "/dashboard/profile",
+    text: "Profile",
+  },
+  {
+    href: "/auth/login",
+    text: "Login",
+  },
+  {
+    href: "/auth/register",
+    text: "Register",
+  },
+];
