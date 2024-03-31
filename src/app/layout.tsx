bea220eb5bb02094760f720aaba9b85371d91e9b
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2024-03-31 10:25:07 -0700

    create manually item to pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/auth/login">Login</Link>
+            </li>
+            <li>
+              <Link href="/auth/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
