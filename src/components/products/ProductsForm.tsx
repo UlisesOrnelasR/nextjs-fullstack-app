146b715fe58e0a9a3d3f6065aa13579393049597
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2024-04-03 18:48:42 -0700

    base form create products

--- src/components/products/ProductsForm.tsx
+++ src/components/products/ProductsForm.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
+import { Label, Input, Button } from "@/components/ui";
 
 const ProductsForm = () => {
-  return <div>ProductsForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Label>Name of product</Label>
+        <Input {...register("name")} />
+        <Label>Description of product</Label>
+        <Input {...register("description")} />
+        <Label>Price of product</Label>
+        <Input {...register("price")} />
+        <Label>Image of product</Label>
+        <Input {...register("image")} />
+        <Button className="block mt-2">Create product</Button>
+      </form>
+    </div>
+  );
 };
 
 export default ProductsForm;
