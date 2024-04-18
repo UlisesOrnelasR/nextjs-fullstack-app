d17d2c2b020d42275ca05359b997d3930e6391ee
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2024-04-18 16:05:38 -0700

    new design landing

--- src/routes/navbarRoutes.ts
+++ src/routes/navbarRoutes.ts
@@ -6,30 +6,19 @@ import {
 
 export const navbarRoutes = [
   {
-    href: "/dashboard",
-    text: "Dashboard",
-    auth: true,
-    // role: ["admin","moderador"] Lo va a ver un admin y moderador, se hace con condicional para controlarlo
-  },
-  {
-    href: "/dashboard/profile",
-    text: "Profile",
-    auth: true,
-  },
-  {
     href: "/dashboard/products",
     text: "Products",
-    auth: true,
+    // auth: true,
   },
   {
     href: "/auth/login",
     text: "Login",
-    auth: false,
+    // auth: false,
   },
   {
     href: "/auth/register",
     text: "Register",
-    auth: false,
+    // auth: false,
   },
 ];
 
